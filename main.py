902908ad3bda1d7ae1eafbc93612fefcddfd9a4b
kenichi_hikita <kenichi_hikita@VTLT0129.local>
2020-05-21 16:05:48 +0900

    first

---
+++ main.py
@@ -0,0 +1,27 @@
+import os
+import json
+import os
+from pprint import pprint
+import responder
+import requests
+
+api = responder.API()
+
+PA_URL = os.environ['PA＿URL']
+
+@api.route('/')
+async def index(req,resp):
+    if req.method == "get":
+        resp.text = "Zoom"
+
+    if req.method == "post":
+        req_data = await req.media()
+        print(req_data)
+        headers = {'content-type': 'application/json'}
+        response = requests.post(PA_URL, data=json.dumps(req_data) ,headers=headers)
+
+
+
+if __name__ == '__main__':
+    api.run(address='0.0.0.0',debug=True)
+
